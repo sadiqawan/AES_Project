e8d523c5e753105b45dc9789e292a929c66ef7d2
sadiq awan <awansadiq09@gmail.com>
2025-03-22 20:40:05 +0500

    firebase Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
-import 'package:ase/ui/mobile_ui/dashbord_views/home_view/home_view.dart';
 import 'package:ase/ui/mobile_ui/splash_view/splash_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart'; // Import GetX properly
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 import 'constant/cont_colors.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
